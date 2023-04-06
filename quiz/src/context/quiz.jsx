464c6ab92b4417508def3011f3fb347be262a018
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-04-06 00:05:51 -0300

    Update quiz.jsx

--- quiz/src/context/quiz.jsx
+++ quiz/src/context/quiz.jsx
@@ -5,15 +5,29 @@ const STATES = ['Start', 'Playing', 'End']
 
 const inicialState = {
     gameStage: STATES[0],
-    questions
-}
+    questions,
+    currentQuestion: 0,
+};
 
 const quizReducer = (state, action) => {
     console.log(state, action);
 
     switch(action.type){
         case 'CHANGE_STATE':
-            return state;
+            return {
+                ...state,
+                gameStage: STATES[1],
+            };
+        
+        case 'REORDER_QUESTIONS':
+            const reorderedQuestions = questions.sort(() => {
+                return Math.random() - 0.5;
+            });
+
+            return {
+                ...state,
+                questions: reorderedQuestions,
+            };
         
         default:
             return state;
@@ -23,7 +37,7 @@ const quizReducer = (state, action) => {
 export const QuizContext = createContext();
 
 export const QuizProvide = ({children}) => {
-    const value = useReducer(quizReducer);
+    const value = useReducer(quizReducer, inicialState);
 
     return <QuizContext.Provider value={value}>{children}</QuizContext.Provider>;
 };
