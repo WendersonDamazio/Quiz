de2312d46aee5c9fde9d2f5378a8fd7255bd3366
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-04-06 00:05:46 -0300

    Update Question.jsx

--- quiz/src/componets/Question.jsx
+++ quiz/src/componets/Question.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useContext } from 'react';
+import { QuizContext } from '../context/quiz';
+
+import "./Question.css";
 
 const Question = () => {
-  return (
-    <div>Question</div>
-  )
-}
+  const [quizState, dispatch] = useContext(QuizContext);
+  const currentQuestion = quizState.questions[quizState.currentQuestion]
 
-export default Question
+  return (
+  <div id='question'>
+    <p>
+      Pergunta {quizState.currentQuestion + 1} de {quizState.questions.length}
+    </p>
+    <h2>{currentQuestion.question}</h2>
+    <div id="options-container">
+      <p>Opções</p>
+    </div>
+    <button>Continuar</button>
+  </div>
+  );
+};
+
+export default Question;
