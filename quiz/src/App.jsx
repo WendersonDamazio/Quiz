0e335c760594aa9f4ebd654d7b90b2fa4f476191
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-04-06 00:05:41 -0300

    Update App.jsx

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,13 +1,23 @@
+import { useContext, useEffect } from 'react';
+import { QuizContext } from './context/quiz';
+
 import Welcome from './componets/Welcome';
+import Question from './componets/Question';
 
 import './App.css'
 
 function App() {
+  const [quizState, dispatch] = useContext(QuizContext);
+
+  useEffect(() => {
+    dispatch({type: 'REORDER_QUESTIONS'});
+  },[]);
 
   return (
     <div className="App">
       <h1>Quiz de Progamação</h1>
-      <Welcome/>
+      {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Playing" && <Question />}
     </div>
   )
 }
