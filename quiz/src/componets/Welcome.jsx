55c041823b339cc02cc7c574b185d5184fe02637
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-04-06 00:05:48 -0300

    Update Welcome.jsx

--- quiz/src/componets/Welcome.jsx
+++ quiz/src/componets/Welcome.jsx
@@ -6,17 +6,17 @@ import quiz from '../img/quiz.svg';
 
 import './Welcome.css';
 
-const Wencone = () => {
-  const [quizState, dispatch] = useContext(QuizContext)
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
   
   return (
     <div id='welcome'>
         <h2>Seja bem-vindo</h2>
         <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
+        <button onClick={() => dispatch({type: "CHANGE_STATE"})}>Iniciar</button>
         <img src={quiz} alt="Inicio do Quiz" />
     </div>
   )
 }
 
-export default Wencone
+export default Welcome
