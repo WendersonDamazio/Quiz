a1b062124cf31e9b5d53ddd82889cd90560c6b95
Wenderson Damazio <wendersonmendes60@gmail.com>
2023-05-02 11:51:03 -0300

    Update App.jsx

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from 'react';
 import { QuizContext } from './context/quiz';
 
 import Welcome from './componets/Welcome';
+import GameOver from './componets/GameOver';
 import Question from './componets/Question';
 
 import './App.css'
@@ -18,6 +19,7 @@ function App() {
       <h1>Quiz de Progamação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   )
 }
